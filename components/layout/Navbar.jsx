bc10f294750ad93d4a2c9a3759604ee21f282b9d
Ali Kalout <63719442+alikk351@users.noreply.github.com>
2021-09-10 17:14:48 +0300

    fix router + name appear

--- components/layout/Navbar.jsx
+++ components/layout/Navbar.jsx
@@ -1,17 +1,20 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
+import { useRouter } from 'next/router';
 
 const Navbar = () => {
     const [user, setUser] = useState('');
+    const router = useRouter();
 
     useEffect(() => {
         if (localStorage?.getItem('profile'))
             setUser(JSON.parse(localStorage?.getItem('profile'))?.profile?.user);
-    }, []);
+    }, [router?.asPath]);
 
     return (
         <AppBar position="static" className="display-flex">
